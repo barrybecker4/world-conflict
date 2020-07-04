541609a77976c63ce9ffcf4d71ac040467e68306
Barry Becker <barrybecker4@gmail.com>
2020-07-04 05:34:54 -0700

    restructuring playOneMove so that it will work in client server manner

--- js/client/playOneMove.js
+++ js/client/playOneMove.js
@@ -14,40 +14,50 @@ import makeMove from './makeMove.js';
 const $ = domUtils.$;
 
 
-// Deals with responding to user actions.
+// Deals with responding to user actions - whterh human or AI.
 export default function playOneMove(state) {
 
     oneAtaTime(CONSTS.MOVE_DELAY, function() {
 
         // keep track of the states and last player, and when the player changes, if that player is an AI,
         // play forward all the moves/state changes all at once.
-        // 1) if controllingPlayer is human, then do as below (but store the newStats in an array).
+        // 1) if controllingPlayer is human, then do as below (but store the newState in an array).
         //    when done simulate call out to server where we pass the human moves just made
         // 2) if not human, then call out to server with []
         // 3) when returning from server call, wait 1 second, then request the latest moves from the server
         // 4) play forward whatever moves are retrieved and keep requesting more, until the state indicates
         //    that the controlling player is human again.
-        var controllingPlayer = state.activePlayer();
+        var player = state.activePlayer();
 
-        // let the player pick their move using UI or AI
-        pickMove(controllingPlayer, state, function(move) {
-            // AI makes sounds when playing
-            if (controllingPlayer.personality)
+        // automatically end the turn of dead players
+        if (!state.regionCount(player))
+            return reportMoveCallback(new EndMove());
+
+        if (!player.personality) {
+            uiPickMove(player, state, function(move) {
+                var newState = makeMove(state, move);
+
+                if (newState.endResult) { // did the game end?
+                    showEndGame(newState);
+                    return;
+                } else {
+                    undoManager.setPreviousState(state.copy());
+                    setTimeout(() => playOneMove(newState), 1);   // recursive call to continue
+                }
+            });
+        }
+        else { // AI player
+            aiPlay.aiPickMove(player, state, function(move) {
                 audio.playSound(SOUNDS.CLICK);
+                var newState = makeMove(state, move);
 
-            // the move is chosen - update state to a new immutable copy
-            var newState = makeMove(state, move);
-
-            if (newState.endResult) { // did the game end?
-                showEndGame(newState);
-                return;
-            }
-            else {
-                undoManager.setPreviousState(state.copy());
-                // still more of the game to go - next move, please!
-                setTimeout(() => playOneMove(newState), 1);   // recursive call
-            }
-        });
+                if (newState.endResult) { // did the game end?
+                    showEndGame(newState);
+                    return;
+                }
+                else setTimeout(() => playOneMove(newState), 1);
+            });
+        }
 
         gameRenderer.updateDisplay(state);
     });
@@ -73,22 +83,4 @@ function showEndGame(state) {
 
         uiCallbacks.setBuildCB(gameInitialization.runSetupScreen);
     });
-}
-
-/**
- * Asks the UI (for humans) or the AI (for CPU players) to pick
- * the next move to make in the game. This happens asynchronously.
- *
- * @param player the player to move
- * @param state the state in which to make the move
- * @param reportMoveCallback should be called with the desired move as parameter once the decision is made
- */
-function pickMove(player, state, reportMoveCallback) {
-    // automatically end the turn of dead players
-    if (!state.regionCount(player))
-        return reportMoveCallback(new EndMove());
-
-    // delegate to whoever handles this player
-    let doPickMove = player.personality ? aiPlay.aiPickMove : uiPickMove;
-    doPickMove(player, state, reportMoveCallback);
 }
