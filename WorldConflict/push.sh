bfc5ff89ccb83395820533b2740735b15ede3696
Barry G Becker <barrybecker4@gmail.com>
2025-05-04 08:56:27 -0700

    Use semantic html tags

--- WorldConflict/push.sh
+++ WorldConflict/push.sh
@@ -2,8 +2,11 @@
 # copy common source files to client side so that they can be shared before calling "clasp push"
 { echo "<script>"; cat server/utils/utils.gs; echo "</script>"; } > client/js/utils/utils.js.html
 { echo "<script>"; cat server/utils/sequenceUtils.gs; echo "</script>"; } > client/js/utils/sequenceUtils.js.html
+
+# Create directories if they don't exist
 mkdir -p client/js/state
 mkdir -p client/js/state/classes
+
 { echo "<script>"; cat server/makeMove.gs; echo "</script>"; } > client/js/makeMove.js.html
 { echo "<script>"; cat server/state/gameData.gs; echo "</script>"; } > client/js/state/gameData.js.html
 { echo "<script>"; cat server/state/CONSTS.gs; echo "</script>"; } > client/js/state/CONSTS.js.html
@@ -18,5 +21,5 @@ firestore=$(<server/persistence/firestore.txt)
 firestorePrivateKey=$(<server/persistence/firestorePrivateKey.txt)
 echo "${firestore//%PRIVATE_KEY%/$firestorePrivateKey}" > server/persistence/firestore.gs
 
-# append to
+# Push to Google Apps Script
 clasp push
